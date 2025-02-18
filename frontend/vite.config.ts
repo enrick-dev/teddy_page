30c2f800dbbd4750544a6a8a3737906b0edeb616
Enrick Santos <enricksantos03@gmail.com>
2025-02-18 01:45:28 -0300

    feat: adicionado Dockerfiles e docker-compose no frontend e backend; adicionado package.json e docker-compose na raiz para controlar os diferentes projetos

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    host: true,
+    port: 5173,
+  },
   plugins: [react(), tailwindcss()],
 });
